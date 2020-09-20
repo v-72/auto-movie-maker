f288589792a2edce84acc2214b0b9c8a4bbe13df
Vinay <vinay@tripp.biz>
2020-09-20 16:43:37 +0530

    Read image files from image_folder

--- utils/video.py
+++ utils/video.py
@@ -1,18 +1,14 @@
 import os
 from moviepy.editor import *
-import cv2
 
 def create_video(
     image_folder="images",
     output="output.mp4",
     output_fps=24
     ):
-    img  = [image_folder+'/'+img for img in os.listdir(image_folder) if img.endswith(".jpg")]
-    [preprocess_image(i) for i in img]
-    clips = [ImageClip(m).set_duration(5).crossfadein( 0.5).crossfadeout(0.5)
-      for m in img]
-    [preprocess_image(i) for i in clips]
-    clips = [c.resize( (1920, 1080) ) for c in clips]
+    images  = get_image_files(image_folder)
+    clips = [generate_image_clip(m) for m in images]
+    # clips = [c.resize( (1920, 1080) ) for c in clips]
     clipStart = VideoFileClip(image_folder+"/tip10intro.mov")
     clipEnd = VideoFileClip(image_folder+"/tip10end.mov")
     clips.append(clipEnd)
@@ -20,9 +16,16 @@ def create_video(
     concat_clip = concatenate_videoclips(clips, method="compose")
     concat_clip.write_videofile(output, fps=output_fps)
 
+"""
+Generate movie clip
+"""
+def generate_image_clip(image):
+    image_clip = ImageClip(image).set_duration(5).crossfadein( 0.5).crossfadeout(0.5)
+    return image_clip
+"""
+    Preprocess image
+"""
 def preprocess_image(image):
-    # im = cv2.imread(image)
-    print(dir(image))
     pass
 
 def intro_video():
@@ -36,3 +39,16 @@ def add_text_to_image():
 
 def add_transition():
     pass
+
+"""
+Get image files from image folder
+"""
+def get_image_files(image_folder):
+    return [
+        image_folder+'/'+image 
+        for image 
+        in os.listdir(image_folder) if (
+                image.endswith(".jpg") or
+                image.endswith(".jpeg")
+            )
+        ]
