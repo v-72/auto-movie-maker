bf1b2ca3c475eac6262e5eb7e7e1d9e0c9130a1f
Vinay <vinay@tripp.biz>
2020-09-20 16:22:12 +0530

    CHnages

--- utils/video.py
+++ utils/video.py
@@ -1,5 +1,6 @@
 import os
 from moviepy.editor import *
+import cv2
 
 def create_video(
     image_folder="images",
@@ -7,9 +8,10 @@ def create_video(
     output_fps=24
     ):
     img  = [image_folder+'/'+img for img in os.listdir(image_folder) if img.endswith(".jpg")]
-    clips = [ImageClip(m).set_duration(5)
+    [preprocess_image(i) for i in img]
+    clips = [ImageClip(m).set_duration(5).crossfadein( 0.5).crossfadeout(0.5)
       for m in img]
-
+    [preprocess_image(i) for i in clips]
     clips = [c.resize( (1920, 1080) ) for c in clips]
     clipStart = VideoFileClip(image_folder+"/tip10intro.mov")
     clipEnd = VideoFileClip(image_folder+"/tip10end.mov")
@@ -18,7 +20,9 @@ def create_video(
     concat_clip = concatenate_videoclips(clips, method="compose")
     concat_clip.write_videofile(output, fps=output_fps)
 
-def preprocess_image():
+def preprocess_image(image):
+    # im = cv2.imread(image)
+    print(dir(image))
     pass
 
 def intro_video():
